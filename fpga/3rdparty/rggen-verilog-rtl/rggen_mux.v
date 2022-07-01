29f78b419dd527f276c8daba60bf6520b83f0179
Roman Dobrodii <ztcoils@gmail.com>
2022-07-01 17:01:30 +0200

    FPGA: rggen - simplify rggen_mux - Yosys has problems with recursion in reduce_or for some reason... - TODO: is my implementation correct?

--- fpga/3rdparty/rggen-verilog-rtl/rggen_mux.v
+++ fpga/3rdparty/rggen-verilog-rtl/rggen_mux.v
@@ -6,62 +6,21 @@ module rggen_mux #(
   input   [WIDTH*ENTRIES-1:0] i_data,
   output  [WIDTH-1:0]         o_data
 );
-  function automatic [WIDTH-1:0] __reduce_or;
-    input integer             n;
-    input integer             offset;
-    input [ENTRIES*WIDTH-1:0] data;
 
-    integer           next_n;
-    integer           next_offset;
-    reg [WIDTH-1:0]   result[0:1];
-  begin
-    if (n > 4) begin
-      next_n      = n / 2;
-      next_offset = offset;
-      result[0]   = __reduce_or(next_n, next_offset, data);
+  wire [ENTRIES*WIDTH-1:0] masked_data;
+  wire [ENTRIES*WIDTH-1:0] orred_data;
 
-      next_n      = (n / 2) + (n % 2);
-      next_offset = (n / 2) + offset;
-      result[1]   = __reduce_or(next_n, next_offset, data);
+  genvar i;
+  generate
+    for (i = 0; i < ENTRIES; i = i + 1) begin
+        assign masked_data[i*WIDTH+:WIDTH] = {WIDTH{i_select[i]}} & i_data[i*WIDTH+:WIDTH];
+        if ( i == 0 ) begin
+          assign orred_data[i*WIDTH+:WIDTH] = masked_data[i*WIDTH+:WIDTH];
+        end else begin
+          assign orred_data[i*WIDTH+:WIDTH] = orred_data[(i-1)*WIDTH+:WIDTH] | masked_data[i*WIDTH+:WIDTH];
+        end
+    end
+  endgenerate
 
-      __reduce_or = result[0] | result[1];
-    end
-    else if (n == 4) begin
-      __reduce_or = data[(0+offset)*WIDTH+:WIDTH] | data[(1+offset)*WIDTH+:WIDTH]
-                  | data[(2+offset)*WIDTH+:WIDTH] | data[(3+offset)*WIDTH+:WIDTH];
-    end
-    else if (n == 3) begin
-      __reduce_or = data[(0+offset)*WIDTH+:WIDTH] | data[(1+offset)*WIDTH+:WIDTH]
-                  | data[(2+offset)*WIDTH+:WIDTH];
-    end
-    else if (n == 2) begin
-      __reduce_or = data[(0+offset)*WIDTH+:WIDTH] | data[(1+offset)*WIDTH+:WIDTH];
-    end
-    else begin
-      __reduce_or = data[(0+offset)*WIDTH+:WIDTH];
-    end
-  end
-  endfunction
-
-  function automatic [WIDTH-1:0] mux;
-    input [ENTRIES-1:0]       select;
-    input [WIDTH*ENTRIES-1:0] data;
-
-    integer                 i;
-    reg [ENTRIES*WIDTH-1:0] masked_data;
-  begin
-    if (ENTRIES > 1) begin
-      for (i = 0;i < ENTRIES;i = i + 1) begin
-        masked_data[i*WIDTH+:WIDTH] = {WIDTH{select[i]}} & data[i*WIDTH+:WIDTH];
-      end
-
-      mux = __reduce_or(ENTRIES, 0, masked_data);
-    end
-    else begin
-      mux = data[0+:WIDTH];
-    end
-  end
-  endfunction
-
-  assign  o_data  = mux(i_select, i_data);
+  assign  o_data  = orred_data[(ENTRIES-1)*WIDTH+:WIDTH];
 endmodule
