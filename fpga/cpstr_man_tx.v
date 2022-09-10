d6a1cb6029e7656e8ee750f055cd5fcdd3682ac4
Roman Dobrodii <ztcoils@gmail.com>
2022-09-10 16:27:06 +0200

    FPGA: implement cpstr_man_tx module - "Control Port Tx Stream Manager" - Stream multiplexer with RR arbiter and burst limit

---
+++ fpga/cpstr_man_tx.v
@@ -0,0 +1,155 @@
+module cpstr_man_tx #(
+    parameter NUM_STREAMS = 2,
+    parameter MAX_BURST = 127
+) (
+    // syscon
+    input i_clk,
+    input i_rst,
+    // input streams
+    input [8*NUM_STREAMS-1:0] i_data,
+    input [NUM_STREAMS-1:0] i_valid,
+    output [NUM_STREAMS-1:0] o_ready,
+    // output (multiplexed) stream
+    output [7:0] o_data,
+    output o_valid,
+    input i_ready,
+    // request to send stream index
+    input i_emit_stridx
+);
+
+    integer ii;
+
+    wire clk, rst;
+    assign clk = i_clk;
+    assign rst = i_rst;
+
+    // arbiter module (from verilog-wishbone)
+    wire [NUM_STREAMS-1:0] req_others;
+    wire [NUM_STREAMS-1:0] req;
+    wire [NUM_STREAMS-1:0] grant;
+    wire [NUM_STREAMS-1:0] grant_ack;
+    wire [$clog2(NUM_STREAMS)-1:0] grant_idx;
+
+    assign req_others = i_valid & ~grant;
+    // normally request follows `i_valid`, but if burst counter signals end
+    // of the burst, it switches to `req_others` to make arbiter switch
+    assign req = burst_rem ? i_valid : req_others;
+
+    /*
+     * Some notes:
+     *  grant[i]==1 means stream `i` is selected
+     *  grant is one-hot, only a single bit can be 1
+     *  grant_idx is equal to `i` or `0` if grant==0
+     * With ARB_BLOCK=1, ARB_BLOCK_ACK=0 arbiter arbitrates whenever request
+     * is _de-asserted_. That's how we trigger arbitration when burst counter
+     * signals end of burst.
+     */
+    arbiter #(
+        .PORTS(NUM_STREAMS),
+        .ARB_BLOCK(1),
+        .ARB_BLOCK_ACK(0),
+        .ARB_TYPE_ROUND_ROBIN(1)
+    ) arbiter (
+        .clk(i_clk),
+        .rst(i_rst),
+        //
+        .request(req),
+        .grant(grant),
+        .grant_encoded(grant_idx)
+    );
+
+    // multiplexed stream
+    // valid and ready are gated by stridx_confirmed, so that mux stream
+    // becomes active only when stridx has been confirmed
+    reg [7:0] mux_data;
+    wire mux_valid;
+    wire mux_ready;
+
+    assign mux_valid = stridx_confirmed && |(i_valid & grant);
+
+    always @(*) begin
+        mux_data = 8'd0;
+        for (ii = 0; ii < NUM_STREAMS; ii=ii+1)
+            if (grant[ii])
+                mux_data = mux_data | i_data[8*ii +: 8];
+    end
+
+    // upstream ready - same gating logic as for mux_valid
+    assign o_ready = grant &
+                    {NUM_STREAMS{mux_ready && stridx_confirmed}};
+
+    // burst byte counter
+    reg [$clog2(MAX_BURST)-1:0] burst_rem;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst || !req_others || !burst_rem)
+            // initialize counter to MAX_BURST on reset or when there are no
+            // competing streams (thus no burst limitation if only one stream
+            // wants to transmit data)
+            burst_rem <= MAX_BURST - 1;
+        else if (mux_valid && mux_ready)
+            // decrement for each byte transmitted over 'muxed' stream
+            burst_rem <= burst_rem - 1'd1;
+    end
+
+    // Stream idx confirmation machine
+    // When arbiter selects a new stream, we use cpstr_esc's 'emit' mechanism
+    // to send {ESC_CHAR, grant_idx} to the host. While that pair of bytes is
+    // in process of being sent, incoming streams are blocked.
+    // This machine reacts to any change in grant_idx on arbiter's output.
+    wire [7:0] stridx_grant;    // index selected by arbiter
+    reg [7:0] stridx_cfrm;      // confirmed index
+    reg [7:0] stridx_emit;      // index to emit
+    reg stridx_emit_valid;
+    wire stridx_emit_ready;
+
+    assign stridx_grant = grant_idx;
+
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            stridx_cfrm <= 8'd0;
+            stridx_emit <= 8'd0;
+            stridx_emit_valid <= 1'b1;
+        end else begin
+            if (stridx_emit_valid) begin
+                if (stridx_emit_ready) begin
+                    // record confirmed index, clr valid for at least 1 cycle
+                    stridx_cfrm <= stridx_emit;
+                    stridx_emit_valid <= 1'b0;
+                end
+            end else begin
+                if ((!stridx_confirmed && grant) || i_emit_stridx) begin
+                    // send stridx to the host
+                    stridx_emit <= stridx_grant;
+                    stridx_emit_valid <= 1'b1;
+                end
+            end
+        end
+    end
+
+    // when stridx is not confirmed, mux stream should be gated so that no
+    // data passes till the correct stridx is sent to host
+    wire stridx_confirmed;
+    assign stridx_confirmed = (stridx_grant == stridx_cfrm) &&
+                              !stridx_emit_valid;
+
+    // stream escaper
+    // via 'emit' mechanism it sends stream idx that was selected by arbiter
+    cpstr_esc cpstr_esc (
+        .i_clk(clk),
+        .i_rst(rst),
+        //
+        .i_data(mux_data),
+        .i_valid(mux_valid),
+        .o_ready(mux_ready),
+        //
+        .o_data(o_data),
+        .o_valid(o_valid),
+        .i_ready(i_ready),
+        //
+        .i_emit_data(stridx_emit),
+        .i_emit_valid(stridx_emit_valid),
+        .o_emit_ready(stridx_emit_ready)
+    );
+
+endmodule
---
+++ fpga/test/test_cpstr_man_tx.v
@@ -0,0 +1,179 @@
+
+`timescale 1 ns/100 ps
+
+module stream_gen (
+    input i_clk,
+    input i_enable,
+    input i_send_esc,
+
+    input i_ready,
+    output [7:0] o_data,
+    output o_valid
+);
+
+    reg [7:0] d;
+
+    assign o_valid = i_enable;
+    assign o_data = i_send_esc ? 8'd27 : d;
+
+    initial begin
+        d <= 8'd0;
+    end
+
+    always @(posedge i_clk) begin
+        if (o_valid && i_ready) begin
+            // Transaction happens, increment
+            d <= d + 8'd1;
+        end
+    end
+
+endmodule
+
+module test_cpstr_man_tx;
+    stream_gen gen1 (
+        .i_clk(clk),
+        .i_enable(en[0]),
+        .i_send_esc(send_esc),
+        .i_ready(src_ready[0]),
+        .o_data(src_data[0 +: 8]),
+        .o_valid(src_valid[0])
+    );
+
+    stream_gen gen2 (
+        .i_clk(clk),
+        .i_enable(en[1]),
+        .i_send_esc(send_esc),
+        .i_ready(src_ready[1]),
+        .o_data(src_data[8 +: 8]),
+        .o_valid(src_valid[1])
+    );
+
+    stream_gen gen3 (
+        .i_clk(clk),
+        .i_enable(en[2]),
+        .i_send_esc(send_esc),
+        .i_ready(src_ready[2]),
+        .o_data(src_data[16 +: 8]),
+        .o_valid(src_valid[2])
+    );
+
+    cpstr_man_tx #(
+        .NUM_STREAMS(3),
+        .MAX_BURST(4)
+    ) dut (
+        .i_clk(clk),
+        .i_rst(rst),
+        //
+        .i_data(src_data),
+        .i_valid(src_valid),
+        .o_ready(src_ready),
+        //
+        .o_data(dst_data),
+        .o_valid(dst_valid),
+        .i_ready(dst_ready),
+        //
+        .i_emit_stridx(emit_stridx)
+    );
+
+    reg clk = 0;
+    reg rst = 0;
+    reg send_esc = 0;
+    reg [2:0] en = 0;
+
+    wire [23:0] src_data;
+    wire [2:0] src_valid;
+    wire [2:0] src_ready;
+
+    wire [7:0] dst_data;
+    wire dst_valid;
+    reg dst_ready = 0;
+
+    reg emit_stridx = 0;
+
+    always #5 clk = ~clk;
+
+    integer i;
+    genvar jj;
+    generate
+        for (jj = 0; jj < 3; jj = jj + 1) begin
+            always @(posedge en[jj])
+                $display("ENABLE %1d", jj);
+            always @(negedge en[jj])
+                $display("DISABLE %1d", jj);
+        end
+    endgenerate
+
+    always @(posedge clk) begin
+        for (i = 0; i < 3; i=i+1) begin
+            if (src_valid[i] && src_ready[i]) begin
+                $display("SRC %1d: 0x%02x", i, src_data[8*i +: 8]);
+            end
+        end
+        if (dst_valid && dst_ready) begin
+            $display("DST:       0x%02x", dst_data);
+        end
+    end
+
+    initial begin
+        $dumpfile("test_cpstr_man_tx.vcd");
+        $dumpvars;
+
+        repeat (5) @(posedge clk);
+        rst <= 1;
+        repeat (1) @(posedge clk);
+        rst <= 0;
+        // activate str 0
+        en[0] <= 1;
+        repeat (4) @(posedge clk);
+        dst_ready <= 1;
+        repeat (10) @(posedge clk);
+        // activate str 1, check arbitrage
+        en[1] <= 1;
+        repeat (40) @(posedge clk);
+        // activate str 2, check arbitrage
+        en[2] <= 1;
+        repeat (44) @(posedge clk);
+        dst_ready <= 0;
+        repeat (10) @(posedge clk);
+        dst_ready <= 1;
+        repeat (6) @(posedge clk);
+        // deactivate all, activate 1
+        en <= 0;
+        en[1] <= 1;
+        repeat (10) @(posedge clk);
+        // deactivate all, activate 1 with delay
+        en <= 0;
+        repeat (5) @(posedge clk);
+        en[1] <= 1;
+        // wait for ESC_CHAR and then disable stream
+        @(posedge clk);
+        while(!(src_valid[1] && src_ready[1] && src_data[8+:8] == 8'd27))
+            @(posedge clk);
+        // deactivate 1, enable 2
+        en <= 0;
+        en[2] <= 1;
+        repeat (20) @(posedge clk);
+        // deactivate 2, enable 1
+        en <= 0;
+        en[1] <= 1;
+        repeat (10) @(posedge clk);
+        // send strind in the middle of the stream
+        emit_stridx <= 1'b1;
+        @(posedge clk);
+        emit_stridx <= 1'b0;
+        repeat (10) @(posedge clk);
+        // activate 2 for one byte
+        en[2] <= 1;
+        @(posedge clk); while(!(src_valid[2] & src_ready[2])) @(posedge clk);
+        // activate 0 for one byte
+        en <= 0;
+        en[0] <= 1;
+        @(posedge clk); while(!(src_valid[0] & src_ready[0])) @(posedge clk);
+        // deactivate
+        en <= 0;
+        repeat (10) @(posedge clk);
+
+        $finish;
+    end
+
+endmodule
